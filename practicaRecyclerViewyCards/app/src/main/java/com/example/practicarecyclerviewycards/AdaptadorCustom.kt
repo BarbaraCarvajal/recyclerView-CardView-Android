61b4ee7b2d720e273d2239d5e8bdf8e3c931fb1f
Barbara Carvajal <barb.carvajalsaez@gmail.com>
2023-04-11 00:27:35 -0400

    avanzado, revisar porque se repiten los datos

---
+++ practicaRecyclerViewyCards/app/src/main/java/com/example/practicarecyclerviewycards/AdaptadorCustom.kt
@@ -0,0 +1,50 @@
+package com.example.practicarecyclerviewycards
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdaptadorCustom: RecyclerView.Adapter<AdaptadorCustom.ViewHolder>() {
+
+    val titles = arrayOf("Barbara", "Mokita", "Camilin", "Programacion")
+
+    val details = arrayOf("Kotlin", "Python", "JavaScript", "Java")
+
+    val images = arrayOf(R.drawable.ic_launcher_foreground,
+                         R.drawable.ic_launcher_foreground,
+                         R.drawable.ic_launcher_foreground,
+                         R.drawable.ic_launcher_foreground)
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(ViewHolder: ViewHolder, i: Int) {
+        ViewHolder.itemTitle.text = titles[1]
+        ViewHolder.itemDetail.text = details[1]
+        ViewHolder.itemImage.setImageResource(images[1])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init{
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+
+        }
+    }
+}
--- practicaRecyclerViewyCards/app/src/main/java/com/example/practicarecyclerviewycards/MainActivity.kt
+++ practicaRecyclerViewyCards/app/src/main/java/com/example/practicarecyclerviewycards/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.practicarecyclerviewycards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = AdaptadorCustom()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+
     }
+
 }
